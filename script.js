7475a8301d9cd40e9acd23b8bb60f3ffa2878cc6
embe35 <147669248+embe35@users.noreply.github.com>
2024-03-21 11:59:51 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('Hello world');
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+}
+
+document.getElementById("year");
+document.getElementById("year").innerHTML = new Date().getFullYear();
